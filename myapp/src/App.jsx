3e3af77a242cfd5cbc0eae71c5caf047601cfdad
Home-PC <danielsh95@gmail.com>
2024-08-10 23:40:29 +0300

    initial mobile support

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -7,29 +7,22 @@ import { Route, Routes } from "react-router-dom";
 import FullProfileComp from "./Home/Profile/FullProfile";
 import ContactPage from "./Pages/ContactPage/ContactPage";
 import AccessibilityPage from "./Pages/AccessibilityPage/AccessibilityPage";
+import { useMediaQuery } from 'react-responsive';
+import MobileHomeComp from "./Mobile/HomePageMobile/MobileHomeComp";
 
 function App() {
-  // const users = useSelector((state) => state.users.users);
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-    // נטען את הנתונים בהתחלת הרכיב
-    dispatch(usersActions.load('Avi'));
-  }, [dispatch]);
+  const isDesktop = useMediaQuery({ minWidth: 769 });
 
   return (
     <div>
-      {/* {users} <br />
-      <button onClick={() => { dispatch(usersActions.load(parseInt(Math.random() * 100))) }}>Click me</button> */}
 
-      {/* <HomeComp/> */}
 
       <Routes>
-        {/* Dynamic Routing - Params */}
-        <Route path='/' element={<HomeComp />} />
-        <Route path='/profile/:profileId' element={<FullProfileComp />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/accessibility' element={<AccessibilityPage />} />
+        <Route path='/' element={isDesktop ? <HomeComp /> : <MobileHomeComp />} />
+        <Route path='/profile/:profileId' element={isDesktop ? <FullProfileComp /> : <MobileHomeComp />} />
+        <Route path='/contact' element={isDesktop ? <ContactPage /> : <MobileHomeComp />} />
+        <Route path='/accessibility' element={isDesktop ? <AccessibilityPage /> : <MobileHomeComp />} />
       </Routes>
 
 
---
+++ myapp/src/Mobile/HomePageMobile/MobileHomeComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileHomeComp = () => {
+    return (
+        <div>
+            MobileHomeComp123
+        </div>
+    )
+}
+
+export default MobileHomeComp
